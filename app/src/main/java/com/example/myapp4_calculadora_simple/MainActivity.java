ba30355bac03ad0ad2dcc7b5fb584fdd0f064a0e
guimainini <guimaini@gmail.com>
2020-06-09 08:43:52 -0300

    parte grafica

---
+++ app/src/main/java/com/example/myapp4_calculadora_simple/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapp4_calculadora_simple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
